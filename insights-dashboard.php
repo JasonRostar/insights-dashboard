20ced4c37252be792905950a079078e0d934d029
Jason Rostar <jason.rostar@yahoo.com>
2026-01-01 17:14:14 -0500

    Initial plugin scaffold and activation

---
+++ insights-dashboard.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Site Insights Dashboard
+ * Description: Adds a React-powered admin dashboard for site insights.
+ * Version: 0.1.0
+ * Author: Jason Rostar
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
